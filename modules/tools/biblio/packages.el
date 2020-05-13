3bb4890296aaf6f815330757694a3fe12f5812fd
Brian McGillion <brian.mcgillion@darkmatter.ae>
2020-05-13 12:57:45 +0400

    Bump tools: biblio
    tmalsburg/helm-bibtex@b14b628 -> tmalsburg/helm-bibtex@8a0dd98

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/biblio/packages.el
 
-(package! bibtex-completion :pin "b14b628261")
+(package! bibtex-completion :pin "8a0dd9841316793aacddea744d6b8ca4a7857a35")
 (when (featurep! :completion ivy)
-  (package! ivy-bibtex :pin "b14b628261"))
+  (package! ivy-bibtex :pin "8a0dd9841316793aacddea744d6b8ca4a7857a35"))
 (when (featurep! :completion helm)
-  (package! helm-bibtex :pin "b14b628261"))
+  (package! helm-bibtex :pin "8a0dd9841316793aacddea744d6b8ca4a7857a35"))
 (when (featurep! :lang org)
   (package! org-ref :pin "4ce80644377f2369efb475bd58a57cf6950d8c41"))
 (when (featurep! :lang org +roam)
